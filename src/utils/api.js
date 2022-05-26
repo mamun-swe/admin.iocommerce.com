34bd084f29be8826bfef259518825ba30e8a2b86
Mamun-swe <mhmamun166009@gmail.com>
2022-05-27 01:03:49 +0600

    env example file included

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,8 @@
-export const api = "http://localhost:4000/api/v1/admin/";
-export const authApi = "http://localhost:4000/api/v1/auth/admin/";
-export const aclApi = "http://localhost:4000/api/v1/acl/";
-// export const api = "https://api.eazybest.com/api/v1/admin/";
-// export const api = "https://fakestoreapi.com/";
+
+const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT
+
+export const api = `${API_ENDPOINT}api/v1/admin/`;
+export const authApi = `${API_ENDPOINT}api/v1/auth/admin/`;
+export const aclApi = `${API_ENDPOINT}api/v1/acl/`;
+// export const api = `https://api.eazybest.com/api/v1/admin/`;
+// export const api = `https://fakestoreapi.com/`;
